f79b6df85fe394fd3ba988aabe87c837f0b010c6
charlesaguiar93 <charlesaguiar93@gmail.com>
2025-10-19 00:15:53 -0300

    exercicio-boasvindas-php

--- validacoes.js
+++ validacoes.js
@@ -1,13 +1,17 @@
 function validarFormulario() {
 
 
-let nome = document.getElementById("nome").value.trim();
+let camponome = document.getElementById("nome");
+let nome = camponome.value.trim();
 
 
 
+if (nome === "") {
+    alert("O campo nome é obrigatório.");
+    camponome.focus();
+    return false;
 
-
-
+}
 
 
 }
